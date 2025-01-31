86fa0350514abfd69dde0aa00fe12f2ef4c0bf60
MaliqT <maliq.tepkhoom@wsu.edu>
2025-01-31 00:19:42 -0800

    Slightly improved
    Slightly improved mouse stat display and dialogue manager maybe, idk.

--- piratejam16/scripts/dialogue_manager.gd
+++ piratejam16/scripts/dialogue_manager.gd
@@ -7,39 +7,63 @@ var dialogue_lines: Array[String] = []
 var current_line_index = 0
 
 var text_box
-var text_box_position = Vector2(0,0)
+var text_box_position: Vector2
 
 var is_dialogue_active = false
 var can_advance_line = false
 
+var mouse_position
 
-func start_dialogue(player: Node2D, lines: Array[String]):
+# Don't need Player node anymore since we're not using it
+func start_dialogue(pos: Vector2, lines: Array[String]):
 	if is_dialogue_active:
 		return
 	
 	dialogue_lines = lines
-	_show_text_box()
+	text_box_position = pos
+	_show_text_box(true)
 	
 	is_dialogue_active = true
 
+func pop_up(pos: Vector2, lines: Array[String]):
+	mouse_position = pos
+	
+	if is_instance_valid(text_box):
+		# Check if text box already exists. 
+		_update_to_mouse()
+		text_box.display_text(lines[0], false)
+	else:
+		dialogue_lines = lines
+		# show the text: Not dialogue
+		_show_text_box(false)
 
-func _show_text_box():
+	
+func _show_text_box(is_dialogue):
 	text_box = text_box_scene.instantiate()
-	text_box.finished_displaying.connect(_on_text_box_finished_displaying)
 	get_tree().get_root().add_child(text_box)
 	
-	_update_text_box_position()
-	text_box.display_text(dialogue_lines[current_line_index])
-	can_advance_line = false
-	
+	if is_dialogue:
+		text_box.finished_displaying.connect(_on_text_box_finished_displaying)
+		_update_text_box_position()
+		text_box.display_text(dialogue_lines[current_line_index], is_dialogue)
+		can_advance_line = false
+	else:
+		_update_to_mouse()
+		text_box.display_text(dialogue_lines[current_line_index], is_dialogue)
 
 func _process(delta: float) -> void:
 	if is_dialogue_active and text_box and text_box_position:
 		_update_text_box_position()
+	elif not is_dialogue_active and text_box and text_box_position:
+		_update_to_mouse()
 
 func _update_text_box_position():
 	if text_box and text_box_position:
-		text_box.global_position = text_box_position# + Vector2(-45, -25)
+		text_box.global_position = text_box_position + Vector2(-45, -25)
+
+func _update_to_mouse():
+	if mouse_position:
+		text_box.global_position = mouse_position - Vector2(0, 15)
 
 func _on_text_box_finished_displaying():
 	can_advance_line = true
@@ -54,9 +78,11 @@ func _unhandled_input(event):
 			current_line_index = 0
 			return
 			
-		_show_text_box()
-		
-		
+		_show_text_box(true)
+	elif (event.is_action_pressed("skip_dialogue")):
+		fuck_this_shit()
+
+
 func fuck_this_shit():
 	if is_instance_valid(text_box):
 		current_line_index = 0
